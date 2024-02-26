de69d745bd98edf355405894a0cf279b8c6164e3
Anderson Mann <andersonmann@PPM-SPO-014399.local>
2024-02-26 03:33:35 -0300

    adicionados logs

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -7,6 +7,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.MessagesAndLogs;
 
+import java.util.logging.Logger;
+
 
 /**
  * This class provides the initial parameters to initiate a session
@@ -14,8 +16,10 @@ import utils.MessagesAndLogs;
  * @author andersonmann
  */
 public class BaseTest {
+    private static final Logger LOGGER = Logger.getLogger(BaseTest.class.getName());
+
     protected static WebDriver driver;
-    private static final String url = MessagesAndLogs.URL;
+    private static final String URL = MessagesAndLogs.URL;
 
     public WebDriver getDriver() {
         return driver;
@@ -24,13 +28,17 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
-        driver.get(url);
+        LOGGER.info("Browser instanciado: GOOGLE CHROME");
+        driver.get(URL);
+        LOGGER.info("Página acessada: ".concat((URL)));
+
     }
 
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
             driver.quit();
+            LOGGER.info("Instância do browser finalizada");
         }
     }
 }
